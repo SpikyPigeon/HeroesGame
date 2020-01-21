182045bb5e1a73e4f497e4fff5d9b72d5af0adf9
Daniel Grondin <king@numsgil.co>
2020-01-21 10:09:41 -0500

    Added static serve for frontend in backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,15 @@
+import {ServeStaticModule} from "@nestjs/serve-static";
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import {join} from "path";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, "..", "..", "frontend", "dist"),
+		}),
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {
 }

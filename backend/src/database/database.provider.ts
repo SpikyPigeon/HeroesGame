4fafe00e7cc54fadbc23a7bec8cba35df7afcea7
Daniel Grondin <king@numsgil.co>
2020-01-23 09:36:16 -0500

    Since SQLite does not support enum, moved dev database to postgresql

--- backend/src/database/database.provider.ts
+++ backend/src/database/database.provider.ts
@@ -1,31 +1,19 @@
 import {createConnection} from "typeorm";
-import {join} from "path";
 
 export const databaseProviders = [
 	{
 		provide: "DATABASE_CONNECTION",
-		useFactory: async () => await createConnection(
-			process.env.NODE_ENV === "production" ?
-				{
-					type: "postgres",
-					host: "localhost",
-					username: "heroes",
-					password: "ReactHeroes",
-					database: "heroes",
-					uuidExtension: "pgcrypto",
-					synchronize: true,
-					entities: [
-						__dirname + "/../**/*.entity{.ts,.js}",
-					],
-				} :
-				{
-					type: "sqlite",
-					database: join(__dirname, "../../data/api.sqlite"),
-					synchronize: true,
-					entities: [
-						__dirname + "/../**/*.entity{.ts,.js}",
-					],
-				}
-		)
+		useFactory: async () => await createConnection({
+			type: "postgres",
+			host: "localhost",
+			username: "heroes",
+			password: "ReactHeroes",
+			database: "heroes",
+			uuidExtension: "pgcrypto",
+			synchronize: true,
+			entities: [
+				__dirname + "/../**/*.entity{.ts,.js}",
+			],
+		})
 	}
 ];

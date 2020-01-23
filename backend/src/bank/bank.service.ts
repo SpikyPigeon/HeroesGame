6cdcb3012049d4fa60373c365c903089a5351515
Daniel Grondin <king@numsgil.co>
2020-01-23 14:17:05 -0500

    Forgot to add the Bank's repository to BankService

--- backend/src/bank/bank.service.ts
+++ backend/src/bank/bank.service.ts
@@ -1,5 +1,12 @@
-import {Injectable} from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {Repository} from "typeorm";
+import {BankEntity} from "./bank.entity";
 
 @Injectable()
 export class BankService {
+	constructor(
+		@Inject("BANK_REPOSITORY")
+		private readonly squares: Repository<BankEntity>
+	) {
+	}
 }

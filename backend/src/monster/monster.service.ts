8b9e118f614d43f6aef1b60d820dd98d2198f7e7
Daniel Grondin <king@numsgil.co>
2020-01-23 14:15:31 -0500

    Damn copy-pasta... forgot to change inject names

--- backend/src/monster/monster.service.ts
+++ backend/src/monster/monster.service.ts
@@ -1,5 +1,15 @@
-import {Injectable} from "@nestjs/common";
+import {Inject, Injectable} from "@nestjs/common";
+import {Repository} from "typeorm";
+import {MonsterTypeEntity} from "./monster-type.entity";
+import {MonsterEntity} from "./monster.entity";
 
 @Injectable()
 export class MonsterService {
+	constructor(
+		@Inject("MONSTER_REPOSITORY")
+		private readonly monsters: Repository<MonsterEntity>,
+		@Inject("MONSTER_TYPE_REPOSITORY")
+		private readonly types: Repository<MonsterTypeEntity>,
+	) {
+	}
 }

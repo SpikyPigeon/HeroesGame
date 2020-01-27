07f20e5151acea5126b36dbb7aa6f6a442786805
Alexis <exerapidoxthorn@gmail.com>
2020-01-27 11:13:36 -0500

    Woopsies on Postsies

--- backend/src/world/world.controller.ts
+++ backend/src/world/world.controller.ts
@@ -1,5 +1,22 @@
-import {Controller} from "@nestjs/common";
+import {Body, Controller, Post} from "@nestjs/common";
+import {WorldService} from "./world.service";
+import {WorldEntity} from "./world.entity";
+
+interface CreateWorldInfo{
+	name: string;
+	X: number;
+	Y: number;
+	color: string;
+	bgImage: string;
+}
 
 @Controller()
 export class WorldController {
+	constructor(private readonly worlds: WorldService) {
+	}
+
+	@Post()
+	async create(@Body() data: CreateWorldInfo):Promise<WorldEntity>{
+		return await this.worlds.create(data.name, data.X, data.Y, data.color, data.bgImage);
+	}
 }

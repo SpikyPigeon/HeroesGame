01c0ceefee1599b315a5e89bcff64262d03462dc
Daniel Grondin <king@numsgil.co>
2020-01-28 11:07:14 -0500

    Added stuff to the RollController (which works!)

--- backend/src/item/roll.controller.ts
+++ backend/src/item/roll.controller.ts
@@ -1,5 +1,24 @@
-import {Controller} from "@nestjs/common";
+import {Controller, Get, Param, Post} from "@nestjs/common";
+import {RollService} from "./roll.service";
+import {RollEntity} from "./roll.entity";
 
 @Controller("/roll")
 export class RollController {
+	constructor(private readonly rolls: RollService) {
+	}
+
+	@Get()
+	async getAll(): Promise<Array<RollEntity>> {
+		return await this.rolls.findAll();
+	}
+
+	@Get(":id")
+	async getOne(@Param("id") id: string): Promise<RollEntity> {
+		return await this.rolls.findOne(id);
+	}
+
+	@Post(":item")
+	async create(@Param("item") item: number): Promise<RollEntity> {
+		return await this.rolls.create(item);
+	}
 }

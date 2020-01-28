476fdab85e33be806179f9195197edb5052696ca
Daniel Grondin <king@numsgil.co>
2020-01-28 10:18:25 -0500

    Tried some stuff in RollService for roll generation... might work

--- backend/src/item/roll.service.ts
+++ backend/src/item/roll.service.ts
@@ -1,15 +1,36 @@
 import {Inject, Injectable} from '@nestjs/common';
 import {Repository} from "typeorm";
-import {ItemEntity} from "./item.entity";
+import {ItemService} from "./item.service";
 import {RollEntity} from "./roll.entity";
 
 @Injectable()
 export class RollService {
 	constructor(
-		@Inject("ITEM_REPOSITORY")
-		private readonly items: Repository<ItemEntity>,
 		@Inject("ROLL_REPOSITORY")
 		private readonly rolls: Repository<RollEntity>,
+		private readonly items: ItemService,
 	) {
+	}
+
+	async findAll(): Promise<Array<RollEntity>> {
+		return await this.rolls.find();
+	}
+
+	async findOne(id: string): Promise<RollEntity> {
+		return await this.rolls.findOneOrFail(id);
+	}
+
+	async create(itemId: number): Promise<RollEntity> {
+		const roll = this.rolls.create({
+			item: await this.items.findOneItem(itemId),
+		});
+
+		Object.keys(roll.item).map(key => {
+			if (key.endsWith("Mod") && Reflect.get(roll.item, key) > 0) {
+				Reflect.set(roll, key.replace("Mod", "Mult"), Math.random() + Math.random());
+			}
+		});
+
+		return await this.rolls.save(roll);
 	}
 }

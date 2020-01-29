8a011aab3d0998c6fe999c2196d3241c49f70c5b
Daniel Grondin <king@numsgil.co>
2020-01-29 16:58:29 -0500

    Managed to make Guards work on Monster

--- backend/src/monster/monster.controller.ts
+++ backend/src/monster/monster.controller.ts
@@ -1,9 +1,11 @@
-import {Body, Controller, Get, Param, Post, Put} from "@nestjs/common";
-import {ApiBody, ApiCreatedResponse, ApiOkResponse, ApiTags} from "@nestjs/swagger";
+import {ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiOkResponse, ApiTags} from "@nestjs/swagger";
+import {Body, Controller, Get, Param, Post, Put, UseGuards} from "@nestjs/common";
+import {AuthGuard} from "@nestjs/passport";
 import {MonsterInfo, MonsterTypeInfo} from "./monster.dto";
 import {MonsterTypeEntity} from "./monster-type.entity";
 import {MonsterService} from "./monster.service";
 import {MonsterEntity} from "./monster.entity";
+import {AdminGuard} from "../user";
 
 @ApiTags("monster")
 @Controller()
@@ -23,15 +25,19 @@ export class MonsterController {
 		return await this.monsters.findOneType(id);
 	}
 
+	@ApiBearerAuth()
 	@ApiCreatedResponse({type: MonsterTypeEntity})
 	@ApiBody({type: MonsterTypeInfo})
+	@UseGuards(AuthGuard("jwt"), AdminGuard)
 	@Post("type")
 	async createType(@Body() data: MonsterTypeInfo): Promise<MonsterTypeEntity> {
 		return await this.monsters.createType(data.name, data.description);
 	}
 
+	@ApiBearerAuth()
 	@ApiOkResponse({type: MonsterTypeEntity})
 	@ApiBody({type: MonsterTypeInfo})
+	@UseGuards(AuthGuard("jwt"), AdminGuard)
 	@Put("type/:id")
 	async updateType(@Param("id") id: number, @Body() data: Partial<MonsterTypeInfo>): Promise<MonsterTypeEntity> {
 		return await this.monsters.updateType(id, data);
@@ -49,15 +55,19 @@ export class MonsterController {
 		return await this.monsters.findOneMonster(id);
 	}
 
+	@ApiBearerAuth()
 	@ApiCreatedResponse({type: MonsterEntity})
 	@ApiBody({type: MonsterInfo})
+	@UseGuards(AuthGuard("jwt"), AdminGuard)
 	@Post()
 	async createMonster(@Body() data: MonsterInfo): Promise<MonsterEntity> {
 		return await this.monsters.createMonster(data);
 	}
 
+	@ApiBearerAuth()
 	@ApiOkResponse({type: MonsterEntity})
 	@ApiBody({type: MonsterInfo})
+	@UseGuards(AuthGuard("jwt"), AdminGuard)
 	@Put(":id")
 	async updateMonster(@Param("id") id: number, @Body() data: Partial<MonsterInfo>): Promise<MonsterEntity> {
 		return await this.monsters.updateMonster(id, data);

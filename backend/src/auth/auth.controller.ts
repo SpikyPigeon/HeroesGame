cfb1b6cb95013429d46a10d446cea14b87f5602d
Daniel Grondin <king@numsgil.co>
2020-01-29 16:28:56 -0500

    Reordered some decorators in AuthController

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -11,20 +11,21 @@ export class AuthController {
 	constructor(private readonly auth: AuthService) {
 	}
 
+	@ApiOkResponse({description: "User is now logged in", type: LoginResponse})
+	@ApiForbiddenResponse({description: "Login information are incorrect"})
 	@ApiBody({type: LoginDefinition})
 	@UseGuards(AuthGuard("local"))
 	@Post("login")
-	@ApiOkResponse({description: "User is now logged in", type: LoginResponse})
-	@ApiForbiddenResponse({description: "Login information are incorrect"})
 	async login(@UserRequest() user: UserEntity): Promise<LoginResponse> {
 		return this.auth.login(user);
 	}
 
-	@UseGuards(AuthGuard("jwt"))
 	@ApiBearerAuth()
-	@Post("passwd")
 	@ApiOkResponse({description: "Password is changed"})
 	@ApiForbiddenResponse({description: "Token expired or token is invalid"})
+	@ApiBody({type: PasswordChangeDefinition})
+	@UseGuards(AuthGuard("jwt"))
+	@Post("passwd")
 	async changePassword(@UserRequest() user: UserEntity, @Body() data: PasswordChangeDefinition) {
 		try {
 			await this.auth.changePassword(user.id, data.oldPass, data.newPass);

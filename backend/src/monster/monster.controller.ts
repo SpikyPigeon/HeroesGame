62695a867cee6d364fd8a7689ea8e26ead5eb9aa
Daniel Grondin <king@numsgil.co>
2020-01-29 12:00:52 -0500

    Added Api doc to Monster

--- backend/src/monster/monster.controller.ts
+++ backend/src/monster/monster.controller.ts
@@ -1,48 +1,63 @@
 import {Body, Controller, Get, Param, Post, Put} from "@nestjs/common";
-import {MonsterInfo, MonsterService, TypeInfo} from "./monster.service";
-import {MonsterEntity} from "./monster.entity";
+import {ApiBody, ApiCreatedResponse, ApiOkResponse, ApiTags} from "@nestjs/swagger";
+import {MonsterInfo, MonsterTypeInfo} from "./monster.dto";
 import {MonsterTypeEntity} from "./monster-type.entity";
+import {MonsterService} from "./monster.service";
+import {MonsterEntity} from "./monster.entity";
 
+@ApiTags("monster")
 @Controller()
 export class MonsterController {
 	constructor(private readonly monsters: MonsterService) {
 	}
 
+	@ApiOkResponse({type: MonsterTypeEntity, isArray: true})
 	@Get("type")
-	async findAllTypes(): Promise<MonsterTypeEntity[]> {
+	async findAllTypes(): Promise<Array<MonsterTypeEntity>> {
 		return await this.monsters.findAllTypes();
 	}
 
+	@ApiOkResponse({type: MonsterTypeEntity})
 	@Get("type/:id")
 	async findOneType(@Param("id") id: number): Promise<MonsterTypeEntity> {
 		return await this.monsters.findOneType(id);
 	}
 
+	@ApiCreatedResponse({type: MonsterTypeEntity})
+	@ApiBody({type: MonsterTypeInfo})
 	@Post("type")
-	async createType(@Body() data: TypeInfo): Promise<MonsterTypeEntity> {
+	async createType(@Body() data: MonsterTypeInfo): Promise<MonsterTypeEntity> {
 		return await this.monsters.createType(data.name, data.description);
 	}
 
+	@ApiOkResponse({type: MonsterTypeEntity})
+	@ApiBody({type: MonsterTypeInfo})
 	@Put("type/:id")
-	async updateType(@Param("id") id: number, @Body() data: Partial<TypeInfo>): Promise<MonsterTypeEntity> {
+	async updateType(@Param("id") id: number, @Body() data: Partial<MonsterTypeInfo>): Promise<MonsterTypeEntity> {
 		return await this.monsters.updateType(id, data);
 	}
 
+	@ApiOkResponse({type: MonsterEntity, isArray: true})
 	@Get()
 	async findAllMonsters(): Promise<MonsterEntity[]> {
 		return await this.monsters.findAllMonsters();
 	}
 
+	@ApiOkResponse({type: MonsterEntity})
 	@Get(":id")
 	async findOneMonster(@Param("id") id: number): Promise<MonsterEntity> {
 		return await this.monsters.findOneMonster(id);
 	}
 
+	@ApiCreatedResponse({type: MonsterEntity})
+	@ApiBody({type: MonsterInfo})
 	@Post()
 	async createMonster(@Body() data: MonsterInfo): Promise<MonsterEntity> {
 		return await this.monsters.createMonster(data);
 	}
 
+	@ApiOkResponse({type: MonsterEntity})
+	@ApiBody({type: MonsterInfo})
 	@Put(":id")
 	async updateMonster(@Param("id") id: number, @Body() data: Partial<MonsterInfo>): Promise<MonsterEntity> {
 		return await this.monsters.updateMonster(id, data);
--- backend/src/monster/monster.service.ts
+++ backend/src/monster/monster.service.ts
@@ -1,24 +1,8 @@
 import {Inject, Injectable} from "@nestjs/common";
 import {Repository} from "typeorm";
+import {MonsterInfo, MonsterTypeInfo} from "./monster.dto";
 import {MonsterTypeEntity} from "./monster-type.entity";
 import {MonsterEntity} from "./monster.entity";
-
-export interface MonsterInfo {
-	name: string,
-	description: string,
-	type: MonsterTypeEntity,
-	level: number,
-	strength: number,
-	dexterity: number,
-	vitality: number,
-	intellect: number,
-	picture: string,
-}
-
-export interface TypeInfo {
-	name: string,
-	description: string,
-}
 
 @Injectable()
 export class MonsterService {
@@ -30,80 +14,50 @@ export class MonsterService {
 	) {
 	}
 
-	//CREATE Section
-	async createMonster(data: MonsterInfo): Promise<MonsterEntity> {
-		return await this.monsters.save(this.monsters.create({...data}));
-	}
-
-	async createType(name: string, description: string): Promise<MonsterTypeEntity> {
-		const type = await this.types.save(this.types.create({
-			name,
-			description,
-		}));
-		await this.types.create(type);
-		return type;
-	}
-
-	//FIND ALL Section
-	async findAllMonsters(): Promise<MonsterEntity[]> {
-		return await this.monsters.find();
-	}
-
 	async findAllTypes(): Promise<MonsterTypeEntity[]> {
 		return await this.types.find();
-	}
-
-	//FIND ONE Section
-	async findOneMonster(id: number): Promise<MonsterEntity> {
-		return await this.monsters.findOneOrFail({where: {id}});
 	}
 
 	async findOneType(id: number): Promise<MonsterTypeEntity> {
 		return await this.types.findOneOrFail({where: {id}});
 	}
 
-	//UPDATE Section
-	async updateMonster(id: number, newMonster: Partial<MonsterInfo>): Promise<MonsterEntity> {
-		const monster = await this.findOneMonster(id);
-		if (newMonster.name) {
-			monster.name = newMonster.name;
-		}
-		if (newMonster.description) {
-			monster.description = newMonster.description;
-		}
-		if (newMonster.type) {
-			monster.type = newMonster.type;
-		}
-		if (newMonster.level) {
-			monster.level = newMonster.level;
-		}
-		if (newMonster.strength) {
-			monster.strength = newMonster.strength;
-		}
-		if (newMonster.dexterity) {
-			monster.dexterity = newMonster.dexterity;
-		}
-		if (newMonster.vitality) {
-			monster.vitality = newMonster.vitality;
-		}
-		if (newMonster.intellect) {
-			monster.intellect = newMonster.intellect;
-		}
-		if (newMonster.picture) {
-			monster.picture = newMonster.picture;
-		}
-		return await this.monsters.save(monster);
+	async createType(name: string, description: string): Promise<MonsterTypeEntity> {
+		return await this.types.save(this.types.create({
+			name,
+			description,
+		}));
 	}
 
-	async updateType(id: number, newType: Partial<TypeInfo>): Promise<MonsterTypeEntity> {
-		const type = await this.findOneType(id);
-		if (newType.name) {
-			type.name = newType.name;
-		}
-		if (newType.description) {
-			type.description = newType.description;
-		}
+	async updateType(id: number, newType: Partial<MonsterTypeInfo>): Promise<MonsterTypeEntity> {
+		const type = {...await this.findOneType(id), ...newType};
 		return await this.types.save(type);
 	}
 
+	async findAllMonsters(): Promise<MonsterEntity[]> {
+		return await this.monsters.find();
+	}
+
+	async findOneMonster(id: number): Promise<MonsterEntity> {
+		return await this.monsters.findOneOrFail({where: {id}});
+	}
+
+	async createMonster(data: MonsterInfo): Promise<MonsterEntity> {
+		const {typeId, ...info} = data;
+		return await this.monsters.save(this.monsters.create({
+			type: await this.findOneType(typeId),
+			...info
+		}));
+	}
+
+	async updateMonster(id: number, newMonster: Partial<MonsterInfo>): Promise<MonsterEntity> {
+		const {typeId, ...info} = newMonster;
+		const monster = {...await this.findOneMonster(id), ...info};
+
+		if (typeId) {
+			monster.type = await this.findOneType(typeId);
+		}
+
+		return await this.monsters.save(monster);
+	}
 }

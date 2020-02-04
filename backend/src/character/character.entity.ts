833025d1ca9f9e4f321f84d7e804898cdd968e24
Daniel Grondin <king@numsgil.co>
2020-02-04 10:36:10 -0500

    Added Api docs to Character and Equipment Entities

--- backend/src/character/character.entity.ts
+++ backend/src/character/character.entity.ts
@@ -1,3 +1,4 @@
+import {ApiProperty} from "@nestjs/swagger";
 import {
 	Column,
 	CreateDateColumn,
@@ -17,66 +18,87 @@ import {InventoryEntity} from "./inventory";
 
 @Entity("PlayerCharacter")
 export class CharacterEntity implements PlayerCharacter {
+	@ApiProperty()
 	@PrimaryGeneratedColumn("uuid")
 	id!: string;
 
+	@ApiProperty()
 	@ManyToOne(type => UserEntity, user => user.characters)
 	owner!: UserEntity;
 
+	@ApiProperty()
 	@Column({length: 30})
 	name!: string;
 
+	@ApiProperty()
 	@ManyToOne(type => AvatarEntity)
 	avatar!: AvatarEntity;
 
+	@ApiProperty()
 	@ManyToOne(type => SquareEntity)
 	square!: SquareEntity;
 
+	@ApiProperty()
 	@Column({default: 0})
 	experience!: number;
 
+	@ApiProperty()
 	@Column({default: 1})
 	level!: number;
 
+	@ApiProperty()
 	@Column({default: 10})
 	strength!: number;
 
+	@ApiProperty()
 	@Column({default: 10})
 	dexterity!: number;
 
+	@ApiProperty()
 	@Column({default: 10})
 	vitality!: number;
 
+	@ApiProperty()
 	@Column({default: 10})
 	intellect!: number;
 
+	@ApiProperty()
 	@Column()
 	currentHealth!: number;
 
+	@ApiProperty()
 	@Column()
 	currentMana!: number;
 
+	@ApiProperty()
 	@Column({default: 200})
 	currentEnergy!: number;
 
+	@ApiProperty()
 	@Column({default: 0})
 	gold!: number;
 
+	@ApiProperty()
 	@Column({default: 0})
 	gem!: number;
 
+	@ApiProperty()
 	@Column({default: false})
 	isDead!: boolean;
 
+	@ApiProperty()
 	@Column({default: true})
 	isActive!: boolean;
 
+	@ApiProperty()
 	@CreateDateColumn()
 	createdAt!: Date;
 
+	@ApiProperty()
 	@UpdateDateColumn()
 	updatedAt!: Date;
 
+	@ApiProperty()
 	@OneToOne(type => EquipmentEntity, equipment => equipment.player, {
 		eager: true,
 	})

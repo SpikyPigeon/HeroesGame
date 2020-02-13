04a7a55824da9b83394da709f58ab099ad255b75
Alexis <exerapidoxthorn@gmail.com>
2020-02-13 09:45:49 -0500

    create function in slap controller done

--- backend/src/character/slap/slap.controller.ts
+++ backend/src/character/slap/slap.controller.ts
@@ -1,6 +1,26 @@
-import {Controller, Logger} from "@nestjs/common";
+import {Controller, Logger, Param, Post, Request, UseGuards} from "@nestjs/common";
+import {SlapService} from "./slap.service";
+import {CharacterService} from "../character.service";
+import {ApiBearerAuth, ApiOkResponse} from "@nestjs/swagger";
+import {SlapEntity} from "./slap.entity";
+import {AuthGuard} from "@nestjs/passport";
+import {UserEntity} from "../../user";
 
 @Controller()
 export class SlapController {
 	private readonly logger: Logger = new Logger(SlapController.name);
+	constructor(private readonly slaps: SlapService, private readonly characters: CharacterService) {
+	}
+
+	@ApiBearerAuth()
+	@ApiOkResponse({type: SlapEntity})
+	@UseGuards(AuthGuard("jmt"))
+	@Post(":slapped")
+	async create(@Request() req: any, @Param("slapped") slappedId: string): Promise<SlapEntity>{
+		const {id} = await this.characters.findMine((req.user as UserEntity).id);
+		return await this.slaps.create(id, slappedId);
+	}
+
+	
+
 }

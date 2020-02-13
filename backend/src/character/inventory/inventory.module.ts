c8dd74a886c09af86fc66b2573728317b9fce61b
Alexis <exerapidoxthorn@gmail.com>
2020-02-13 13:36:00 -0500

    inventory service: create, findWithOwner and findOne done

--- backend/src/character/inventory/inventory.module.ts
+++ backend/src/character/inventory/inventory.module.ts
@@ -1,4 +1,4 @@
-import {Module} from "@nestjs/common";
+import {forwardRef, Module} from "@nestjs/common";
 import {InventoryController} from "./inventory.controller";
 import {InventoryService} from "./inventory.service";
 import {inventoryProviders} from "./inventory.provider";
--- backend/src/character/inventory/inventory.service.ts
+++ backend/src/character/inventory/inventory.service.ts
@@ -1,12 +1,50 @@
-import {Inject, Injectable} from "@nestjs/common";
+import {Inject, Injectable, OnModuleInit} from "@nestjs/common";
 import {Repository} from "typeorm";
 import {InventoryEntity} from "./inventory.entity";
+import {RollService} from "../../item";
+import {CharacterService} from "../character.service";
+import {ModuleRef} from "@nestjs/core";
 
 @Injectable()
-export class InventoryService {
+export class InventoryService implements OnModuleInit {
+	private rolls!: RollService;
+	private characters!: CharacterService;
+
 	constructor(
 		@Inject("INVENTORY_REPOSITORY")
 		private readonly inventories: Repository<InventoryEntity>,
+		private readonly refs: ModuleRef
 	) {
 	}
+
+	onModuleInit() {
+		this.characters = this.refs.get(CharacterService, {strict: false});
+		this.rolls = this.refs.get(RollService, {strict: false});
+	}
+
+	async create(rollId: string, ownerId: string, quantity: number): Promise<InventoryEntity> {
+		return await this.inventories.save(this.inventories.create({
+			roll: await this.rolls.findOne(rollId),
+			owner: await this.characters.findMine(ownerId),
+			quantity: quantity
+		}));
+	}
+
+	async findAllWithCharacter(ownerId: string): Promise<Array<InventoryEntity>>{
+		return await this.inventories.createQueryBuilder("inventory")
+			.leftJoinAndSelect("inventory.owner", "owner")
+			.where("owner.id = inventory.ownerId", {ownerId})
+			.getMany();
+	}
+
+	async findOne(id: string): Promise<InventoryEntity>{
+		return await this.inventories.findOneOrFail(id);
+	}
+
+
+
+	/*async update(quantity: number): Promise<InventoryEntity>{
+
+	}*/
+
 }

e7150e3137e6b33b607905c63131dfc8c20b90f7
Daniel Grondin <king@numsgil.co>
2020-02-13 14:51:55 -0500

    Moved the monster's card to its own component and added a health bar

--- frontend/src/routes/game/world.action.tsx
+++ frontend/src/routes/game/world.action.tsx
@@ -1,11 +1,72 @@
-import {Card, CardActionArea, CardContent, CardHeader, CardMedia, Grid} from "@material-ui/core";
-import {createElement, FunctionComponent, useEffect} from "react";
-import {useList} from "react-use";
+import {createElement, FunctionComponent, useEffect, useState} from "react";
+import {red} from "@material-ui/core/colors";
+import {useList, useUpdate} from "react-use";
+import {
+	Card,
+	CardActionArea,
+	CardContent,
+	CardHeader,
+	CardMedia,
+	createStyles,
+	Grid,
+	LinearProgress,
+	makeStyles,
+	Theme
+} from "@material-ui/core";
 
 import {useStoreActions, useStoreState} from "../../store";
 import {config, Encounter, Monster} from "heroes-common";
 
 type MonsterFight = Monster & { health: number };
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		healthBar: {
+			backgroundColor: red["800"],
+		},
+	}),
+);
+
+interface MonsterCardProps {
+	monster: MonsterFight;
+	onFight: (monster: MonsterFight) => void;
+}
+
+const MonsterCard: FunctionComponent<MonsterCardProps> = ({monster, onFight}) => {
+	const [raised, setRaised] = useState(false);
+	const classes = useStyles();
+
+	const {monster: cfg} = config;
+	const maxHealth = cfg.calculate.health(monster.vitality);
+
+	return <Card raised={raised} square>
+		<CardActionArea
+			onMouseEnter={() => setRaised(true)}
+			onMouseLeave={() => setRaised(false)}
+			onClick={() => onFight(monster)}
+		>
+			<CardHeader title={monster.name}
+			            titleTypographyProps={{
+				            align: "center",
+				            variant: "subtitle1",
+				            noWrap: true,
+			            }}
+			            subheader={
+				            <LinearProgress
+					            variant="determinate"
+					            value={monster.health * 100 / maxHealth}
+					            classes={{bar: classes.healthBar}}
+				            />
+			            }
+			/>
+			<CardMedia
+				component="img"
+				image={`/assets/monsters/${monster.picture}`}
+				height={128}
+			/>
+		</CardActionArea>
+	</Card>;
+};
 
 interface WorldActionProps {
 	encounters: Array<Encounter>;
@@ -15,6 +76,7 @@ export const WorldAction: FunctionComponent<WorldActionProps> = ({encounters}) =
 	const updateChar = useStoreActions(state => state.character.update);
 	const character = useStoreState(state => state.character.character);
 	const [monsters, monstersMod] = useList<MonsterFight>([]);
+	const update = useUpdate();
 
 	const spawnMonsters = () => {
 		const {monster} = config;
@@ -36,6 +98,12 @@ export const WorldAction: FunctionComponent<WorldActionProps> = ({encounters}) =
 		}
 	};
 
+	const fight = (monster: MonsterFight) => {
+		console.log(`FIGHT ${monster.name}`);
+		monster.health -= 10;
+		update();
+	};
+
 	useEffect(() => {
 		monstersMod.clear();
 		if (encounters.length > 0) {
@@ -47,25 +115,12 @@ export const WorldAction: FunctionComponent<WorldActionProps> = ({encounters}) =
 		return null;
 	}
 
-	return <Card>
+	return <Card style={{minHeight: 390}}>
 		<CardContent style={{padding: 3}}>
 			<Grid container direction="row" justify="flex-start" spacing={1}>
 				{monsters.map((value, index) => (
 					<Grid item lg={2} key={index}>
-						<Card variant="outlined">
-							<CardActionArea>
-								<CardHeader title={value.name} titleTypographyProps={{
-									align: "center",
-									variant: "subtitle1",
-									noWrap: true,
-								}}/>
-								<CardMedia
-									component="img"
-									image={`/assets/monsters/${value.picture}`}
-									height={128}
-								/>
-							</CardActionArea>
-						</Card>
+						<MonsterCard monster={value} onFight={fight}/>
 					</Grid>
 				))}
 			</Grid>

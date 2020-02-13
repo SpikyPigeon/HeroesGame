3657ef6a2152300e14907470ee9820ae1b07386f
Alexis <exerapidoxthorn@gmail.com>
2020-02-13 10:39:09 -0500

    fixed previous problems with slap controller

--- backend/src/character/slap/slap.controller.ts
+++ backend/src/character/slap/slap.controller.ts
@@ -1,15 +1,26 @@
 import {Controller, Get, Logger, Param, Post, Request, UseGuards} from "@nestjs/common";
 import {SlapService} from "./slap.service";
 import {CharacterService} from "../character.service";
-import {ApiBearerAuth, ApiBody, ApiOkResponse} from "@nestjs/swagger";
+import {ApiBearerAuth, ApiBody, ApiOkResponse, ApiProperty, ApiTags} from "@nestjs/swagger";
 import {SlapEntity} from "./slap.entity";
 import {AuthGuard} from "@nestjs/passport";
 import {UserEntity} from "../../user";
-import {CharacterEntity} from "../character.entity";
 
+export class SlappableDto {
+	@ApiProperty()
+	slappable: boolean = false;
+}
+
+export class SlapCountDto {
+	@ApiProperty()
+	count: number = 0;
+}
+
+@ApiTags("user")
 @Controller()
 export class SlapController {
 	private readonly logger: Logger = new Logger(SlapController.name);
+
 	constructor(private readonly slaps: SlapService, private readonly characters: CharacterService) {
 	}
 
@@ -17,47 +28,50 @@ export class SlapController {
 	@ApiOkResponse({type: SlapEntity})
 	@UseGuards(AuthGuard("jmt"))
 	@Post(":slapped")
-	async create(@Request() req: any, @Param("slapped") slappedId: string): Promise<SlapEntity>{
+	async create(@Request() req: any, @Param("slapped") slappedId: string): Promise<SlapEntity> {
 		const {id} = await this.characters.findMine((req.user as UserEntity).id);
 		return await this.slaps.create(id, slappedId);
 	}
 
 	@ApiOkResponse({type: SlapEntity, isArray: true})
 	@Get()
-	async findAll(): Promise<Array<SlapEntity>>{
+	async findAll(): Promise<Array<SlapEntity>> {
 		return await this.slaps.findAll();
 	}
 
 	@ApiOkResponse({type: SlapEntity, isArray: true})
 	//@ApiBody({type: CharacterEntity})
 	@Get(":id")
-	async findAllWithCharacter(@Param("id") id: string): Promise<Array<SlapEntity>>{
+	async findAllWithCharacter(@Param("id") id: string): Promise<Array<SlapEntity>> {
 		return await this.slaps.findAllWithCharacter(id);
 	}
 
-	//@ApiOkResponse({type: number})
+	@ApiOkResponse({type: SlapCountDto})
 	@Get("count")
-	async countAll(): Promise<number>{
-		return await this.slaps.countAll();
+	async countAll(): Promise<SlapCountDto> {
+		return {
+			count: await this.slaps.countAll()
+		};
 	}
 
+	@ApiOkResponse({type: SlapCountDto})
 	@Get("count/slapped/:id")
-	async countSlapped(@Param("id") id: string): Promise<number>{
-		return await this.slaps.countSlapped(id);
+	async countSlapped(@Param("id") id: string): Promise<SlapCountDto> {
+		return {count: await this.slaps.countSlapped(id)};
 	}
 
+	@ApiOkResponse({type: SlapCountDto})
 	@Get("count/slapper/:id")
-	async countSlapper(@Param("id") id: string): Promise<number>{
-		return await this.slaps.countSlapper(id);
+	async countSlapper(@Param("id") id: string): Promise<SlapCountDto> {
+		return {count: await this.slaps.countSlapper(id)};
 	}
 
 	@ApiBearerAuth()
-	//@ApiOkResponse({type: boolean})
+	@ApiOkResponse({type: SlappableDto})
 	@UseGuards(AuthGuard("jmt"))
 	@Get("slappable/:slappedId")
-	async checkSlappable(@Request() req: any, @Param("slappedId") slappedId: string): Promise<boolean>{
+	async checkSlappable(@Request() req: any, @Param("slappedId") slappedId: string): Promise<SlappableDto> {
 		const {id} = await this.characters.findMine((req.user as UserEntity).id);
-		return await this.slaps.checkSlappable(id, slappedId);
+		return {slappable: await this.slaps.checkSlappable(id, slappedId)};
 	}
-
 }

939fd7d4e1f3523fbd75ab15db9d493654a4bf6b
Alexis <exerapidoxthorn@gmail.com>
2020-02-18 14:27:05 -0500

    Auto stash before merge of "master" and "king-numsgil/master"

--- frontend/src/routes/game/world.feature.tsx
+++ frontend/src/routes/game/world.feature.tsx
@@ -1,5 +1,5 @@
 import {createElement, Fragment, FunctionComponent, useState} from "react";
-import {Card, CardActionArea, CardContent, CardHeader, Grid, Typography} from "@material-ui/core";
+import {Card, CardActionArea, CardContent, CardHeader, CardMedia, Grid, Typography} from "@material-ui/core";
 
 import {Structure} from "heroes-common";
 
@@ -17,45 +17,35 @@ export const FeatureCard: FunctionComponent<FeatureCardProps> = ({structures}) =
 			{structures.map((value, index) => (
 				<Card key={index} style={{minHeight: 100, marginBottom: 8}}>
 					<CardContent style={{padding: 3}}>
-						<Grid container>
-							<Grid item lg={3}>
-								<CardHeader title={value.name}
-								            titleTypographyProps={{
-									            align: "left",
-									            variant: "h5",
-								            }}
-								/>
-							</Grid>
-							<Grid item lg={5}>
-								<Typography align="left">
-									{value.description}
-								</Typography>
-							</Grid>
-							{value.shop && <Grid item lg={4}>
-								<Card raised={raised} square>
-									<CardActionArea
-										onMouseEnter={() => setRaised(true)}
-										onMouseLeave={() => setRaised(false)}
-									>
-										<CardHeader title={"Shop Name"}
-										            titleTypographyProps={{
-											            align: "center",
-											            variant: "h6",
-										            }}
-										/>
-										<Typography align="center">
-											{"Shop description"}
-										</Typography>
-										<Typography align="center" variant="body2">
-											{"Click to shop!"}
-										</Typography>
-									</CardActionArea>
-								</Card>
-							</Grid>}
-						</Grid>
+						<CardHeader title={value.name}
+						            titleTypographyProps={{
+							            align: "left",
+							            variant: "h5",
+						            }}
+						/>
+						<CardMedia
+							component="img"
+							image={`/assets/structures/${value.description}`}
+							height={200}
+						/>
 					</CardContent>
 				</Card>
 			))}
 		</Fragment>;
 	}
 };
+/*{value.shop && <Grid item lg={1}>
+	<Card raised={raised} square>
+		<CardActionArea
+			onMouseEnter={() => setRaised(true)}
+			onMouseLeave={() => setRaised(false)}
+		>
+			<CardHeader title={"Shop Name"}
+			            titleTypographyProps={{
+				            align: "center",
+				            variant: "h6",
+			            }}
+			/>
+		</CardActionArea>
+	</Card>
+</Grid>}*/

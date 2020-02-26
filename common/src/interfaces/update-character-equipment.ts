aa38aa0d6f59a8da1a4e53af9e6e87ba2dc95b7e
Daniel Grondin <king@numsgil.co>
2020-02-26 10:53:04 -0500

    Added interface to Common for Equipment Update

---
+++ common/src/interfaces/update-character-equipment.ts
@@ -0,0 +1,15 @@
+import {ItemRoll} from "./item-roll";
+
+export interface UpdateCharacterEquipment {
+	headSlot: string | null;
+	chestSlot: string | null;
+	beltSlot: string | null;
+	bootSlot: string | null;
+	leftHandSlot: string | null;
+	rightHandSlot: string | null;
+	ring1Slot: string | null;
+	ring2Slot: string | null;
+	neckSlot: string | null;
+	bagSlot: string | null;
+	artifactSlot: string | null;
+}
